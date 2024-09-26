c4ccd3a548496697236848b7b508f49718e9dbd8
Nata <erbet@mail.ru>
2024-09-26 17:46:42 +0300

    add product-card--component

---
+++ src/project/components/product-card/product-card.tsx
@@ -0,0 +1,70 @@
+import { TCamera } from '../../types/types';
+
+type ProductCardProps = {
+  camera: TCamera;
+};
+
+function ProductCard({ camera }: ProductCardProps) {
+  return (
+    <div className="product-card">
+      <div className="product-card__img">
+        <picture>
+          <source
+            type="image/webp"
+            srcSet={camera.previewImg}
+          />
+          <img
+            src={camera.previewImg}
+            srcSet={camera.previewImg2x}
+            width={280}
+            height={240}
+            alt={camera.name}
+          />
+        </picture>
+      </div>
+      <div className="product-card__info">
+        <div className="rate product-card__rate">
+          <svg width={17} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-full-star" />
+          </svg>
+          <svg width={17} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-full-star" />
+          </svg>
+          <svg width={17} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-full-star" />
+          </svg>
+          <svg width={17} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-star" />
+          </svg>
+          <svg width={17} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-star" />
+          </svg>
+          <p className="visually-hidden">Рейтинг: 3</p>
+          <p className="rate__count">
+            <span className="visually-hidden">Всего оценок:</span>23
+          </p>
+        </div>
+        <p className="product-card__title">
+          {camera.name}
+        </p>
+        <p className="product-card__price">
+          <span className="visually-hidden">Цена:</span>{camera.price} ₽
+        </p>
+      </div>
+      <div className="product-card__buttons">
+        <button
+          className="btn btn--purple product-card__btn"
+          type="button"
+        >
+          Купить
+        </button>
+        <a className="btn btn--transparent" href="#">
+          Подробнее
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export { ProductCard };
+
