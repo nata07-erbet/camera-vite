220025864188acab035c693b364984bf827e6cbc
Nata <erbet@mail.ru>
2024-10-02 19:09:13 +0300

    add routing

--- src/project/components/app/app.tsx
+++ src/project/components/app/app.tsx
@@ -1,10 +1,31 @@
-import { Catalog } from '../../pages/catalog';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
 
-function App () {
-  return(
-    <Catalog />
+import { AppRoutes } from '../../const/const';
+
+import { Catalog } from '../../pages/catalog';
+import { Product } from '../../pages/product';
+
+const router = createBrowserRouter([
+  {
+    path: `${AppRoutes.Main}`,
+    element: <Catalog />,
+    children: [
+      {
+        path: `${AppRoutes.Camera}`,
+        element: <Product />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
   );
 }
 
-export { App } ;
+export { App };
 
--- src/project/const/const.ts
+++ src/project/const/const.ts
@@ -11,6 +11,7 @@ const ReqRoutes = {
 
 const AppRoutes = {
   Main: '/',
+  Camera: 'camera/:id'
 };
 
 const NavMap = {
